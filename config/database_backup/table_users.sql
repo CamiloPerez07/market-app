de8818d8ed4afb5953fe5f046615e93615a2984d
Daniel Paz <antonio@gmail.com>
2025-09-02 15:34:56 -0500

    table users

---
+++ config/database_backup/table_users.sql
@@ -0,0 +1,32 @@
+-- Create table users
+create table users(
+    id BIGSERIAL PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    mobile_number VARCHAR(20) NOT NULL,
+    ide_number VARCHAR(15) NULL UNIQUE,
+    address TEXT NULL,
+    birthday DATE NULL,
+    email VARCHAR(200) NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    status BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    deleted_at TIMESTAMPTZ NULL
+);
+
+--Insert into table users
+Insert into users(
+    firstname,
+    lastname,
+    mobile_number,
+    email,
+    password
+)
+values (
+    'Camilo',
+    'Montaño',
+    '3204512022',
+    'camilo@mail.com',
+    '1234'
+)
