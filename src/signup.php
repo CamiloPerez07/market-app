b660a8f5530e55d874ba69efcaab5220d3f907d3
Daniel Paz <antonio@gmail.com>
2025-09-11 10:51:14 -0500

    Encrypt Password

--- src/signup.php
+++ src/signup.php
@@ -9,9 +9,11 @@
     $id_number = $_POST['idnumber'];
     $e_mail = $_POST['email'];
     $p_wd = $_POST['pwd'];
+
+    $enc_pass = password_hash($p_wd, PASSWORD_DEFAULT);
     //Create query to INSERT INTO
     $query = "INSERT INTO users (firstname, lastname, mobile_number, ide_number, email, password) 
-    values ('$f_name','$l_name','$m_number','$id_number','$e_mail','$p_wd')";
+    values ('$f_name','$l_name','$m_number','$id_number','$e_mail','$enc_pass')";
 
     //Execute query
     $res = pg_query($conn,$query);
