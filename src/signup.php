9542955e31063cc853bf4cccc1ca10d59c2dee3c
Daniel Paz <antonio@gmail.com>
2025-09-16 15:35:05 -0500

    integration register user with front-end

--- src/signup.php
+++ src/signup.php
@@ -11,18 +11,38 @@
     $p_wd = $_POST['pwd'];
 
     $enc_pass = password_hash($p_wd, PASSWORD_DEFAULT);
-    //Create query to INSERT INTO
-    $query = "INSERT INTO users (firstname, lastname, mobile_number, ide_number, email, password) 
-    values ('$f_name','$l_name','$m_number','$id_number','$e_mail','$enc_pass')";
 
-    //Execute query
-    $res = pg_query($conn,$query);
-
-    //Validate result
-    if($res){
-        echo "User has been created successfully!!";
+    $check_email = "
+        Select
+            u.email
+        from
+            users u
+        where
+            email= '$e_mail'
+        limit 1
+    ";
+    $res_check = pg_query($conn,$check_email);
+    if(pg_num_rows($res_check)> 0){
+        echo "<script>alert('User already exists')</script>";
+        header('refresh:0;url=signup.html');
     }
     else{
-        echo "Something Wrong....";
+        //Create query to INSERT INTO
+        $query = "INSERT INTO users (firstname, lastname, mobile_number, ide_number, email, password) 
+        values ('$f_name','$l_name','$m_number','$id_number','$e_mail','$enc_pass')";
+
+        //Execute query
+        $res = pg_query($conn,$query);
+        
+        //Validate result
+        if($res){
+            //echo "User has been created successfully!!";
+            echo "<script>alert('Success, Go to login')</script>";
+            header('refresh:0;url=signin.html');
+        }
+        else{
+            echo "<script>alert('User already exists')</script>";
+            header('refresh:0;url=signup.html');
+        }
     }
 ?>
